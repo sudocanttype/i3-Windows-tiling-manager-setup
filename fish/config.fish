3a6b2f075facfc390828c396a1e7298ae63d2f87
oscar <oscarlay789@gmail.com>
2021-02-24 23:19:04 -0600

    fish and starfish configs

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+# Programs
+thefuck --alias | source
+
+set BRUH $PWD
+# Aliases
+alias linuxmasterrace="neofetch --disable cpu gpu memory resolution --color_blocks off | lolcat"
+alias python=python3.6
+alias nv=nvim
+alias tess="tesseract tess.png stdout --psm 6"
+alias ..="cd .."
+alias manage="python manage.py"
+alias sact="source venv/bin/activate"
+alias settings="cinnamon-settings"
+alias nemo="nemo ."
+
+starship init fish | source
