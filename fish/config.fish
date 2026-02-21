4156dddd60b4e12abf985729e35836ac98295d05
ol <oscarlay789@gmail.com>
2026-02-21 13:09:23 -0600

    revert

--- fish/config.fish
+++ fish/config.fish
@@ -3,19 +3,14 @@ thefuck --alias | source
 
 set BRUH $PWD
 # Aliases
-alias linuxmasterrace="neofetch | lolcat"
 alias ..="cd .."
 alias manage="python manage.py"
 alias sact=". venv/bin/activate.fish"
 alias unfuck_wifi="sudo systemctl restart NetworkManager "
 alias ls=exa
 
-fish_vi_key_bindings 
 #bindings
 bind \cd forward-word
-bind \cf accept-autosuggestion
-bind -M insert \cd forward-word
-bind -M insert \cf accept-autosuggestion
 
 
 starship init fish | source
@@ -27,3 +22,6 @@ set -gx CRYPTOGRAPHY_OPENSSL_NO_LEGACY 1
 
 alias activate_conda="eval /opt/miniconda3/bin/conda 'shell.fish' 'hook' $argv | source"
 
+fish_vi_key_bindings
+
+fish_ssh_agent
