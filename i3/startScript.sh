637865da0173a9a3d84ab2b5f0d9ed4e9f25cc72
ol <oscarlay789@gmail.com>
2026-08-21 23:43:03 -0500

    update startscript + remove workspaces

--- i3/startScript.sh
+++ i3/startScript.sh
@@ -2,12 +2,18 @@
 #Makes sure that the monitor config is correct
 MAINMONITOR=HDMI-1
 RIGHTMONITOR=DVI-D-1
-LEFTMONITOR=DP-3
+LEFTMONITOR=DP-1
 # xrandr --output  $MAINMONITOR --primary 
 # xrandr --output $LEFTMONITOR --left-of $MAINMONITOR 
 # xrandr --output $RIGHTMONITOR --right-of $MAINMONITOR --rotate left --scale 1.5x1.5
 
 
+
+# amdgpu fails DP link training on DP-3 coming back from a DPMS power-off, which
+# stalls the atomic commit shared by all three heads and blanks HDMI-1 and DVI-D-1
+# too. Blank the screen instead of powering it down so the DP link is never dropped.
+xset -dpms
+xset s 600 600
 
 xrandr --output $LEFTMONITOR --scale 1.5x1.5 --pos 0x0
 xrandr --output  $MAINMONITOR --primary --pos 2880x0
